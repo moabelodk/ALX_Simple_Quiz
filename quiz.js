05021bc4ddd11a5efe234e85728110f390644e54
moabelo karabo <moabelokarabo0@gmail.com>
2025-06-24 21:22:29 +0200

    added README.md file

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    const correctAnswer = '4' 
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+    const feedback = document.getElementById("feedback");
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
